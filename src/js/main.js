a54df0fc1be877af967af7b0b0ba369dd50abdac
Remco Spindle <remco@Remcos-Spindle-MacBook-Pro-7.local>
2021-04-15 21:46:39 +0200

    added basic svg's and animations

--- src/js/main.js
+++ src/js/main.js
@@ -7,9 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
     let tl =  gsap.timeline();
 
     tl  
-        .set(["#loading_line"], {autoAlpha:0})
+        .set(["#loading_line", "#check_mark","#cross"], {autoAlpha:0})
 
         .add("start")
         .to(["#loading_line"], {autoAlpha:1, duration: 2})
-        .fromTo("#loading_line_path", {drawSVG:"0% 0%"},{duration: 1.8, drawSVG:true, ease: "power1.Out"}, "start+=.2");
+        .fromTo("#loading_line_path", {drawSVG:"0% 0%"},{duration: 1.8, drawSVG:true, ease: "power1.Out"}, "start+=.2")
+        .to(["#check_mark"], {autoAlpha:1, duration: 2})
+        .fromTo("#check_mark_path", {drawSVG:"0% 0%"},{duration: 1.8, drawSVG:true, ease: "power1.Out"}, "start+=2.2")
+        .to(["#cross"], {autoAlpha:1, duration: 2})
+        .fromTo("#cross_stroke_left_path", {drawSVG:"0% 0%"},{duration: 1.8, drawSVG:true, ease: "power1.Out"}, "start+=4.2")
+        .fromTo("#cross_stroke_right_path", {drawSVG:"0% 0%"},{duration: 1.8, drawSVG:true, ease: "power1.Out"}, "start+=4.4");
 });
