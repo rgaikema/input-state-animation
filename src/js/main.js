a227ef78715d5baf0f7e0bf1655b77243fff4a1e
Remco Spindle <remco@Remcos-Spindle-MacBook-Pro-7.local>
2021-04-16 11:13:08 +0200

    Made two separete animations in one SVG

--- src/js/main.js
+++ src/js/main.js
@@ -10,11 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
         .set(["#loading_line", "#check_mark","#cross"], {autoAlpha:0})
 
         .add("start")
-        .to(["#loading_line"], {autoAlpha:1, duration: 2})
+        .to("#loading_line", {autoAlpha:1, duration: 2})
         .fromTo("#loading_line_path", {drawSVG:"0% 0%"},{duration: 1.8, drawSVG:true, ease: "power1.Out"}, "start+=.2")
-        .to(["#check_mark"], {autoAlpha:1, duration: 2})
-        .fromTo("#check_mark_path", {drawSVG:"0% 0%"},{duration: 1.8, drawSVG:true, ease: "power1.Out"}, "start+=2.2")
-        .to(["#cross"], {autoAlpha:1, duration: 2})
-        .fromTo("#cross_stroke_left_path", {drawSVG:"0% 0%"},{duration: 1.8, drawSVG:true, ease: "power1.Out"}, "start+=4.2")
-        .fromTo("#cross_stroke_right_path", {drawSVG:"0% 0%"},{duration: 1.8, drawSVG:true, ease: "power1.Out"}, "start+=4.4");
+
+        .to("#check_mark", {autoAlpha:1, duration: 2})
+        .fromTo("#check_mark_path", {drawSVG:"0% 0%"},{duration: 1.8, drawSVG:true, ease: "power1.Out"}, "start+=2.2");
+
+        // .fromTo("#cross_stroke_left_path", {drawSVG:"0% 0%"},{duration: 1.8, drawSVG:true, ease: "power1.Out"}, "start+=4.2")
+        // .fromTo("#cross_stroke_right_path", {drawSVG:"0% 0%"},{duration: 1.8, drawSVG:true, ease: "power1.Out"}, "start+=4.4");
 });
