89b5021f81367ff68f3c7db9ed5e4facaa93099a
Remco Spindle <remco@Remcos-Spindle-MacBook-Pro-8.local>
2021-04-20 13:47:26 +0200

    chained the check mark animation after loading timeline

--- src/js/main.js
+++ src/js/main.js
@@ -19,16 +19,12 @@ document.addEventListener("DOMContentLoaded", function () {
         return tl;
     };
 
+    // Loading animation
     const rotate = () => {
-
-        // const checkValue = () => {
-        //     const prog = tl.progress();
-        //     if(prog === 1){
-        //         console.log("check");
-        //     }
-        // };
         const valueHolder = document.getElementById("progressValue");
         let counter = 0;
+
+        // After 3x loading add removing line animation
         const checkRepeat = () => {
             counter++;
             if(counter === 3){
@@ -39,39 +35,60 @@ document.addEventListener("DOMContentLoaded", function () {
             valueHolder.innerText = counter;
         };
 
-
+        // Fill loader & check amount of repeats
         const tl = gsap.timeline({repeat: 3, onRepeat: checkRepeat});
-
-         // Rotate
          tl
             .fromTo("#loading_line_path", {transformOrigin: "50% 50%", rotate: "0"}, {duration: 2, rotate: "-360", ease: "power1.Out"});
 
         return tl;
     };
 
+
+    // Completing the loading animation 
     const endLoading = () => {
-        
+
+        // Add loading line removal animation
         const remove = () => {
             tlMain
             .add(removeLoading());
-        }
+        };
 
+        // After filling the circle start the line removal animation
         const tl = gsap.timeline({onComplete: remove});
-
         tl
             .fromTo("#loading_line_path", {drawSVG:"75% 0%"},{duration: .7, drawSVG:"100% 0%", ease: "power1.Out"});
+        
+            return tl;
+    };
 
-        return tl;
-    }
 
+    // Remove the line animation
     const removeLoading = () => {
 
-        const tl = gsap.timeline();
-        tl
-            .fromTo("#loading_line_path", {drawSVG:"100% 0%"},{duration: .7, drawSVG:"0% 0%", ease: "power1.Out"});
+        // Add the check mark animation to the Main timeline after the loading line is gone
+        const checkIn = () => {
+            tlMain
+            .add(check());
+        };
 
-        return tl;
-    }
+        // After the loading line is gone start the check mark animation
+        const tl = gsap.timeline({onComplete: checkIn});
+        tl
+            .fromTo("#loading_line_path", {drawSVG:"100% 0%"},{duration: 1.2, drawSVG:"100% 100%", ease: "power1.Out"});
+        
+            return tl;
+    };
+
+    // Check mark animation In
+    const check = () => {
+        const tl = gsap.timeline();
+
+        // Animate the check mark in
+        tl
+        .to("#check_mark", {autoAlpha:1, duration: .2})
+        .fromTo("#check_mark_path", {drawSVG:"0% 0%"},{duration: 1.8, drawSVG:true, ease: "power1.Out"}, "-=.2");
+
+    };
 
     tlMain
         .add(fadeIn())
