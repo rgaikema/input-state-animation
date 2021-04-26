e527102942d3cd2a8d3b98975a92bc9da9aaea57
Remco Spindle <remco@Remcos-Spindle-MacBook-Pro-9.local>
2021-04-26 09:25:29 +0200

    Shortened the timelines code

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,17 @@
 import { gsap } from "gsap/dist/gsap";
-import { DrawSVGPlugin } from "gsap/dist/DrawSVGPlugin";
+import { DrawSVGPlugin} from "gsap/dist/DrawSVGPlugin";
 gsap.registerPlugin(DrawSVGPlugin);
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    //Place element right position
     const indicator =  document.getElementById("indicator");
     const indicatorWidth = indicator.offsetWidth;
     const optionsElement =  document.getElementById("options-1");
+    const tlMain =  gsap.timeline();
 
-
+    //Place element right position
     indicator.style.height = `${indicatorWidth}px`;
     optionsElement.parentNode.appendChild(indicator);
-
-
-    // Main Timeline
-    const tlMain =  gsap.timeline();
 
     const startLoading = () => {
         const tl =  gsap.timeline();
@@ -25,7 +21,6 @@ document.addEventListener("DOMContentLoaded", function () {
             .set(["#loading_line", "check_mark", "cross"], {opacity:0})
             .to(["#loading_line","#indicator"], {opacity:1, duration: .2})
             .fromTo("#loading_line_path", {drawSVG:"0% 0%"},{duration: 1, drawSVG:"75% 0%", ease: "power1.Out"});
-    
         return tl;
     };
 
@@ -46,27 +41,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Check response during loading
         const checkResponse = () => {
-
             console.log(response);
 
             if(response === "success" || response === "fail"){
-                tl
-                    .pause();
-                tlMain
-                    .add(endLoading(response));
+                tl.pause();
+                tlMain.add(endLoading(response));
             }else{
                 // Recent method is only a return of the last tween, not adding an extra tween
-                tl
-                    .add(tl.recent());
+                tl.add(tl.recent());
             }
         };
 
         // Fill loader & check amount of repeats
         const tl = gsap.timeline({onComplete: checkResponse});
-
         tl
             .fromTo("#loading_line_path", {transformOrigin: "50% 50%", rotate: "0"}, {duration: 1.6, rotate: "-360", ease: "power1.Out"});
-
         return tl;
     };
 
@@ -76,8 +65,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // To start loading line removal animation
         const remove = () => {
-            tlMain
-                .add(removeLoading(response));
+            tlMain.add(removeLoading(response));
         };
 
         // Complete the circle
@@ -85,14 +73,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (response === "success"){
             tl
-            .fromTo("#loading_line_path", {drawSVG:"75% 0%"},{duration: .6, drawSVG:"100% 0%", ease: "power1.Out"});
+                .fromTo("#loading_line_path", {drawSVG:"75% 0%"},{duration: .6, drawSVG:"100% 0%", ease: "power1.Out"});
 
-        } else if (response === "fail"){
+        } else {
             tl
-            .fromTo("#loading_line_path", {transformOrigin: "50% 50%", rotate: "0"}, {duration: 1.6, rotate: "-314", ease: "power1.Out"})
-            .fromTo("#loading_line_path", {drawSVG:"75% 0%"},{duration: .6, drawSVG:"100% 0%", ease: "power1.Out"});
+                .fromTo("#loading_line_path", {transformOrigin: "50% 50%", rotate: "0"}, {duration: 1.6, rotate: "-314", ease: "power1.Out"})
+                .fromTo("#loading_line_path", {drawSVG:"75% 0%"},{duration: .6, drawSVG:"100% 0%", ease: "power1.Out"});
         }
-
         return tl;
     };
 
@@ -104,34 +91,20 @@ document.addEventListener("DOMContentLoaded", function () {
         const startResponseAnimation = () => {
             console.log(response);
 
-            if (response === "success") {
-                tlMain
-                    .add(showCheckMark());
-            } else if(response === "fail"){
-                tlMain
-                    .add(showCross());
-            }else {
-                console.log("error while saving input");
-            }
+            //Continue with appropriate timeline
+            response === "success" ? tlMain.add(showCheckMark()) : tlMain.add(showCross())
         };
-
-        // After the loading line is gone start the check mark animation
-        const tl = gsap.timeline();    
-
+ 
         // Set appropriate class name based on response
         let className;
-        if (response === "success") {
-            className = "+=svg_wrapper success";
-        }else {
-            className = "+=svg_wrapper fail";
-        }
+        response === "success" ? className = "+=svg_wrapper success" : className = "+=svg_wrapper fail";
         
          // Change class name for matching colors based on response & remove loading stroke
+        const tl = gsap.timeline();   
         tl  
             .to (".svg_wrapper", {className: className, duration: .1})
             .fromTo("#loading_line_path", {drawSVG:"100% 0%"},{duration: .8, drawSVG: "100% 100%", ease: "power1.Out"})
             .call(startResponseAnimation);
-
          return tl;
     };
 
@@ -145,7 +118,6 @@ document.addEventListener("DOMContentLoaded", function () {
             .fromTo("#check_mark_path", {drawSVG:"0% 0%"},{duration: 1.8, drawSVG:true, ease: "power1.Out"})
             .to("#check_mark", {opacity:0, duration: .4, ease: "power1.Out"}, "+=1")
             .to("#indicator", {opacity:0, duration: .4, ease: "power1.Out"}, "-=.2");
-
         return tl;
 
     };
@@ -163,6 +135,7 @@ document.addEventListener("DOMContentLoaded", function () {
             .to("#cross", {opacity:0, duration: .4, ease: "power1.Out"}, "+=1")
             .to("#indicator", {opacity:0, duration: .4, ease: "power1.Out"}, "-=.2");
     };
+
 
     // Main timeline starts with loading animation
     tlMain
