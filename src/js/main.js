e6fbd7e8476947edeac32c563a0a4ab95fc0f4c2
Remco Gaikema <remco.gaikema@wearespindle.com>
2021-04-30 13:59:05 +0200

    changed mainTL to timeline per component

--- src/js/main.js
+++ src/js/main.js
@@ -4,142 +4,222 @@ gsap.registerPlugin(DrawSVGPlugin);
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    const indicator =  document.getElementById("indicator");
-    const indicatorWidth = indicator.offsetWidth;
-    const optionsElement =  document.getElementById("options-1");
+    const body = document.getElementsByTagName("body")[0];
+    const indicator =  document.querySelector(".svg_wrapper");
+    // const indicatorWidth = indicator.offsetWidth;
+    // const optionsElement =  document.getElementById("options-1");
     const tlMain =  gsap.timeline();
 
-    //Place element right position
-    indicator.style.height = `${indicatorWidth}px`;
-    optionsElement.parentNode.appendChild(indicator);
+    console.log(gsap.globalTimeline);
 
-    const startLoading = () => {
-        const tl =  gsap.timeline();
+    const components = document.querySelectorAll("[data-component]");
+    let tlNames = [];
 
-        // Start drawing the circle stroke
-        tl  
-            .set(["#loading_line", "check_mark", "cross"], {opacity:0})
-            .to(["#loading_line","#indicator"], {opacity:1, duration: .2})
-            .fromTo("#loading_line_path", {drawSVG:"0% 0%"},{duration: 1, drawSVG:"75% 0%", ease: "power1.Out"});
-        return tl;
-    };
+    console.log(components);
+    components.forEach((element)=> {
+        const id = element.getAttribute("data-component");
+        // console.log(id);
 
-    // Loading animation
-    const loading = () => {
-        let response = "loading";
-        const btnSucces = document.getElementById("btnSuccess");
-        const btnFail = document.getElementById("btnFail");
+        const timelineName = "tl" + id;
+        console.log(timelineName);
 
-        // Check for manual input - TODO replace with promise status
-        btnSucces.addEventListener("click", function() {
-            response = "success";
-        });
-        btnFail.addEventListener("click", function() {
-            response = "fail";
-        });
+        tlNames.push(timelineName);
 
+    });
 
-        // Check response during loading
-        const checkResponse = () => {
-            console.log(response);
+    // console.log(tlNames);
 
-            if(response === "success" || response === "fail"){
-                tl.pause();
-                tlMain.add(endLoading(response));
-            }else{
-                // Recent method is only a return of the last tween, not adding an extra tween
-                tl.add(tl.recent());
-            }
-        };
+    components.forEach((element)=> {
 
-        // Fill loader & check amount of repeats
-        const tl = gsap.timeline({onComplete: checkResponse});
-        tl
-            .fromTo("#loading_line_path", {transformOrigin: "50% 50%", rotate: "0"}, {duration: 1.6, rotate: "-360", ease: "power1.Out"});
-        return tl;
-    };
+        element.addEventListener("click", (e) => {
+            console.log(gsap.globalTimeline);
+            const target = e.target;
+            const rect = target.getBoundingClientRect();
+            const xCor = rect.x - 30;
+            const yCor = rect.y;
+            console.log(xCor, yCor);
 
+            const id = target.getAttribute("data-component");
 
-    // Stop rotating at the right point & complete the loading animation
-    const endLoading = (response) => {
+            let clone = indicator.cloneNode(true);
+            clone.setAttribute("data-id", id);
 
-        // To start loading line removal animation
-        const remove = () => {
-            tlMain.add(removeLoading(response));
-        };
+            body.appendChild(clone);
+            console.log("clone: ", clone)
+            clone.style.left = `${xCor}px`;
+            clone.style.top = `${yCor}px`;
+            clone.style.height = "24px";
+            clone.style.width = "24px";
 
-        // Complete the circle
-        const tl = gsap.timeline({onComplete: remove});
+            element = gsap.timeline();
 
-        if (response === "success"){
-            tl
-                .fromTo("#loading_line_path", {drawSVG:"75% 0%"},{duration: .6, drawSVG:"100% 0%", ease: "power1.Out"});
-
-        } else {
-            tl
-                .fromTo("#loading_line_path", {transformOrigin: "50% 50%", rotate: "0"}, {duration: 1.6, rotate: "-314", ease: "power1.Out"})
-                .fromTo("#loading_line_path", {drawSVG:"75% 0%"},{duration: .6, drawSVG:"100% 0%", ease: "power1.Out"});
-        }
-        return tl;
-    };
+            const iconShapes = clone.querySelector(".loading_line",".check_mark", ".cross");
+            const loadingLine = clone.querySelector(".loading_line");
+            const loadingLinePath = clone.querySelector(".loading_line_path");
+            const checkMark = clone.querySelector(".check_mark");
+            const checkMarkPath = clone.querySelector(".check_mark_path");
+            const cross = clone.querySelector(".cross");
+            const crossLeftPath = clone.querySelector(".cross_stroke_left_path");
+            const crossRightPath = clone.querySelector(".cross_stroke_right_path");
 
 
-    // Remove the line animation
-    const removeLoading = (response) => {
+            const startLoading = () => {
 
-        // To start appropriate timeline
-        const startResponseAnimation = () => {
-            console.log(response);
-
-            //Continue with appropriate timeline
-            response === "success" ? tlMain.add(showCheckMark()) : tlMain.add(showCross());
-        };
- 
-        // Set appropriate class name based on response
-        let className;
-        response === "success" ? className = "+=svg_wrapper success" : className = "+=svg_wrapper fail";
+                const tl =  gsap.timeline();
+                
         
-         // Change class name for matching colors based on response & remove loading stroke
-        const tl = gsap.timeline();   
-        tl  
-            .to (".svg_wrapper", {className: className, duration: .1})
-            .fromTo("#loading_line_path", {drawSVG:"100% 0%"},{duration: .8, drawSVG: "100% 100%", ease: "power1.Out"})
-            .call(startResponseAnimation);
-         return tl;
-    };
+                // Start drawing the circle stroke
+                tl  
+                    .set(iconShapes, {opacity:0})
+                    .to([loadingLine, clone], {opacity:1, duration: .2})
+                    .fromTo(loadingLinePath, {drawSVG:"0% 0%"},{duration: 1, drawSVG:"75% 0%", ease: "power1.Out"});
+                return tl;
+            };
+        
+            // Loading animation
+            const loading = () => {
+                let response = "loading";
+                const btnSucces = document.getElementById("btnSuccess");
+                const btnFail = document.getElementById("btnFail");
+        
+                // Check for manual input - TODO replace with promise status
+                btnSucces.addEventListener("click", function() {
+                    response = "success";
+                });
+                btnFail.addEventListener("click", function() {
+                    response = "fail";
+                });
+        
+        
+                // Check response during loading
+                const checkResponse = () => {
+                    console.log(response);
+        
+                    if(response === "success" || response === "fail"){
+                        tl.pause();
+                        tlMain.add(endLoading(response));
+                    }else{
+                        // Recent method is only a return of the last tween, not adding an extra tween
+                        tl.add(tl.recent());
+                    }
+                };
+        
+                // Fill loader & check amount of repeats
+                // const tl = gsap.timeline({onComplete: () => { console.log('complete')}});
+                const tl = gsap.timeline({onComplete: checkResponse});
+                tl
+                    .fromTo(loadingLinePath, {transformOrigin: "50% 50%", rotate: "0"}, {duration: 1.6, rotate: "-360", ease: "power1.Out"});
+                return tl;
+            };
 
-    //Check mark animation
-    const showCheckMark = () => {
-        const tl = gsap.timeline();
+            // Stop rotating at the right point & complete the loading animation
+            const endLoading = (response) => {
 
-        // Draw check mark
-        tl
-            .to("#check_mark", {opacity:1, duration: .2, ease: "none"})
-            .fromTo("#check_mark_path", {drawSVG:"0% 0%"},{duration: 1.8, drawSVG:true, ease: "power1.Out"})
-            .to("#check_mark", {opacity:0, duration: .4, ease: "power1.Out"}, "+=1")
-            .to("#indicator", {opacity:0, duration: .4, ease: "power1.Out"}, "-=.2");
-        return tl;
+                // To start loading line removal animation
+                const remove = () => {
+                    tlMain.add(removeLoading(response));
+                };
 
-    };
+                // Complete the circle
+                const tl = gsap.timeline({onComplete: remove});
 
-    //Cross animation
-    const showCross = () => {
-        const tl = gsap.timeline();
+                if (response === "success"){
+                    tl
+                        .fromTo(loadingLinePath, {drawSVG:"75% 0%"},{duration: .6, drawSVG:"100% 0%", ease: "power1.Out"});
 
-        // Draw the cross
-        tl
-            .to("#cross", {opacity:1, duration: .2, ease: "none"})
-            .to (".svg_wrapper", {className:"+=svg_wrapper fail", duration: .1})
-            .fromTo("#cross_stroke_left_path", {drawSVG:"0% 0%"},{duration: 1, drawSVG:true, ease: "power1.Out"})
-            .fromTo("#cross_stroke_right_path", {drawSVG:"0% 0%"},{duration: 1, drawSVG:true, ease: "power1.Out"}, "-=.8")
-            .to("#cross", {opacity:0, duration: .4, ease: "power1.Out"}, "+=1")
-            .to("#indicator", {opacity:0, duration: .4, ease: "power1.Out"}, "-=.2");
-    };
+                } else {
+                    tl
+                        .fromTo(loadingLinePath, {transformOrigin: "50% 50%", rotate: "0"}, {duration: 1.6, rotate: "-314", ease: "power1.Out"})
+                        .fromTo(loadingLinePath, {drawSVG:"75% 0%"},{duration: .6, drawSVG:"100% 0%", ease: "power1.Out"});
+                }
+                return tl;
+            };
+
+
+            // Remove the line animation
+            const removeLoading = (response) => {
+
+                // To start appropriate timeline
+                const startResponseAnimation = () => {
+                    console.log(response);
+
+                    //Continue with appropriate timeline
+                    response === "success" ? element.add(showCheckMark()) : element.add(showCross());
+                };
+        
+                // Set appropriate class name based on response
+                let className;
+                response === "success" ? className = "+=svg_wrapper success" : className = "+=svg_wrapper fail";
+                
+                // Change class name for matching colors based on response & remove loading stroke
+                const tl = gsap.timeline();   
+                tl  
+                    .to (clone, {className: className, duration: .1})
+                    .fromTo(loadingLinePath, {drawSVG:"100% 0%"},{duration: .8, drawSVG: "100% 100%", ease: "power1.Out"})
+                    .call(startResponseAnimation);
+                return tl;
+            };
+
+            //Check mark animation
+            const showCheckMark = () => {
+                const tl = gsap.timeline();
+
+                // Draw check mark
+                tl
+                    .to(checkMark, {opacity:1, duration: .2, ease: "none"})
+                    .fromTo(checkMarkPath, {drawSVG:"0% 0%"},{duration: 1.8, drawSVG:true, ease: "power1.Out"})
+                    .to(checkMark, {opacity:0, duration: .4, ease: "power1.Out"}, "+=1")
+                    .to(clone, {opacity:0, duration: .4, ease: "power1.Out"}, "-=.2");
+                return tl;
+
+            };
+
+            //Cross animation
+            const showCross = () => {
+                const tl = gsap.timeline();
+
+                // Draw the cross
+                tl
+                    .to(cross, {opacity:1, duration: .2, ease: "none"})
+                    .to (clone, {className:"+=svg_wrapper fail", duration: .1})
+                    .fromTo(crossLeftPath, {drawSVG:"0% 0%"},{duration: 1, drawSVG:true, ease: "power1.Out"})
+                    .fromTo(crossRightPath, {drawSVG:"0% 0%"},{duration: 1, drawSVG:true, ease: "power1.Out"}, "-=.8")
+                    .to(cross, {opacity:0, duration: .4, ease: "power1.Out"}, "+=1")
+                    .to(clone, {opacity:0, duration: .4, ease: "power1.Out"}, "-=.2");
+            };
+
+
+            element
+            .add(startLoading())
+            .add(loading());
+
+        })
+
+    });
+
+
+
+
+
+    //Place element right position
+    // indicator.style.height = "20px";
+    // indicator.style.width = "20px";
+    //optionsElement.parentNode.appendChild(indicator);
+
+
+
+
+
+
+
+    
+
+
 
 
     // Main timeline starts with loading animation
-    tlMain
-        .add(startLoading())
-        .add(loading());
+   // tlMain
+        // .add(startLoading())
+        // .add(loading());
 
 });
